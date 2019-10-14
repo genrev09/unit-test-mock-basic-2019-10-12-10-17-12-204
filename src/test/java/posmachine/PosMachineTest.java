dcfbd9fa74cd37e8ec436fb17568ebe2dacd8c81
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-14 11:53:38 +0800

    Committed completed test with mocking

--- src/test/java/posmachine/PosMachineTest.java
+++ src/test/java/posmachine/PosMachineTest.java
@@ -1,7 +1,16 @@
 package posmachine;
 
 
+import javafx.geometry.Pos;
+import org.graalvm.compiler.hotspot.stubs.Stub;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.text.CollationKey;
+
+import static org.mockito.Mockito.when;
 
 public class PosMachineTest {
 
@@ -9,22 +18,40 @@ public class PosMachineTest {
     @Test
     public void should_get_receipt_using_real_price_calculator() {
         //given
+        PriceCalculator priceCalculator = new PriceCalculator();
+        PosMachine posMachine = new PosMachine(priceCalculator);
         //when
         //then
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> {
+            posMachine.getReceipt("Coke");
+        });
     }
 
     @Test
-    public void should_get_receipt_using_sub_price_calculator() {
+    public void should_get_receipt_using_stub_price_calculator() {
         //given
-        //when
-        //then
+        String receipt = "Name: Coke, Price: 10.0";
+        PriceCalculator stubCalculator = new StubPriceCalculator();
+        PosMachine posMachine = new PosMachine(stubCalculator);
+        Assertions.assertEquals(receipt, posMachine.getReceipt("Coke"));
     }
 
     @Test
     public void should_get_receipt_using_real_price_calculator_with_mockito() {
-        //given
-        //when
-        //then
+        String receipt = "Name: Coke, Price: 10.0";
+        PriceCalculator calculator = Mockito.mock(PriceCalculator.class);
+        PosMachine posMachine = new PosMachine(calculator);
+        final String productName = "Coke";
+
+        when(calculator.calculate(productName)).thenReturn(10.0);
+
+        Assertions.assertEquals(receipt,posMachine.getReceipt(productName));
     }
 
+    private class StubPriceCalculator extends PriceCalculator {
+        @Override
+        public double calculate(String productName) {
+            return (10.0);
+        }
+    }
 }
